208f67c208f1c2a571e7c5a3be7b0e2447c0597a
Yagiz ALP <yagizwasmeow@gmail.com>
2020-06-11 01:50:46 +0300

    Tries to get rid of ridiculous key locals.

--- client/main.lua
+++ client/main.lua
@@ -1,27 +1,12 @@
--- TODO: Default veriables...
-local Keys      = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
-
-
-local ESX           = nil
-local ped           = PlayerPedId()
-local pedCoords     = {}
-local pedDuty       = false
-local busVehicle    = nil
-local blip          = nil
-local finishMarker  = nil
-local finishBlip    = nil
-local finalBlip     = nil
+local ESX = nil
+local ped = PlayerPedId()
+local pedCoords = {}
+local pedDuty = false
+local busVehicle = nil
+local blip = nil
+local finishMarker = nil
+local finishBlip = nil
+local finalBlip = nil
 
 
 AddEventHandler('playerSpawned', function()
@@ -60,7 +45,7 @@ Citizen.CreateThread(function ()
                     if Vdist(_v.x, _v.y, _v.z, pedCoords.x, pedCoords.y, pedCoords.z) < 5.0 then                
                         ShowPedHelpDialog(_U('finish_marker_msg'))
 
-                        if IsControlPressed(0, Keys['E']) and IsPedInBus() then
+                        if IsControlPressed(0, 38) and IsPedInBus() then
                             FinishBus()
                         end
                     end
@@ -91,7 +76,7 @@ Citizen.CreateThread(function ()
                     if Vdist(_v.x, _v.y, _v.z, pedCoords.x, pedCoords.y, pedCoords.z) < 5.0 then                
                         ShowPedHelpDialog(_U('final_marker_msg'))
 
-                        if IsControlPressed(0, Keys['E']) and IsPedInBus() then
+                        if IsControlPressed(0, 38) and IsPedInBus() then
                             CompleteJob()
                         end
                     end
@@ -119,7 +104,7 @@ Citizen.CreateThread(function()
                     if Vdist(_v.x, _v.y, _v.z, pedCoords.x, pedCoords.y, pedCoords.z) < 1.3 then                
                         ShowPedHelpDialog(_U('duty_help_msg'))
 
-                        if IsControlPressed(0, Keys['E']) then
+                        if IsControlPressed(0, 38) then
                             ShowPedBusDutyMenu()
                         end
                     end
@@ -135,7 +120,7 @@ Citizen.CreateThread(function()
                         if Vdist(_v.x, _v.y, _v.z, pedCoords.x, pedCoords.y, pedCoords.z) < 1.3 then                
                             ShowPedHelpDialog(_U('duty_spawn_bus_msg'))
 
-                            if IsControlPressed(0, Keys['E']) and not IsPedInBus() then
+                            if IsControlPressed(0, 38) and not IsPedInBus() then
                                 SpawnBusForPed()
                             end
                         end
